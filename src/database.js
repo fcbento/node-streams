d642cfb6bc3c3e80c8d2014e363aafe4bd9b6b3c
fcbento <felipe.16costa@gmail.com>
2024-11-13 23:56:51 -0300

    query params

--- src/database.js
+++ src/database.js
@@ -3,8 +3,8 @@ import fs from 'node:fs/promises'
 const databasePath = new URL('../db.json', import.meta.url)
 
 export class Database {
-  
-  #database  = {}
+
+  #database = {}
 
   constructor() {
     fs.readFile(databasePath, 'utf8').then((data) => {
@@ -13,13 +13,22 @@ export class Database {
       this.#persist()
     })
   }
-  
+
   #persist() {
     fs.writeFile(databasePath, JSON.stringify(this.#database))
   }
 
-  select(table) {
-    const data = this.#database[table] ?? []
+  select(table, search) {
+    let data = this.#database[table] ?? []
+    
+    if(search) {
+      data = data.filter(row => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLocaleLowerCase().includes(value.toLocaleLowerCase())
+        })
+      })
+    }
+
     return data
   }
 
@@ -36,8 +45,16 @@ export class Database {
 
   delete(table, id) {
     const rowIndex = this.#database[table].findIndex(row => row.id === id);
-    if(rowIndex > -1) {
+    if (rowIndex > -1) {
       this.#database[table].splice(rowIndex, 1)
+      this.#persist()
+    }
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id);
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = { id, ...data }
       this.#persist()
     }
   }
--- src/routes.js
+++ src/routes.js
@@ -5,13 +5,16 @@ import { buildRoutePath } from './utils/build-route-path.js';
 
 const database = new Database();
 
+
+
 export const routes = [
   {
     method: 'GET',
     path: buildRoutePath('/users'),
     handler: (req, res) => {
-      const users = database.select('users');
-      return res.end(JSON.stringify(users));
+      const { search } = req.query
+      const users = database.select('users', search ? { name: search, email: search } : null)
+      return res.end(JSON.stringify(users))
     }
   },
 
@@ -37,6 +40,15 @@ export const routes = [
     handler: (req, res) => {
       const { id } = req.params
       database.delete('users', id)
+      return res.writeHead(204).end();
+    }
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      database.update('users', id, req.body)
       return res.writeHead(204).end();
     }
   },
