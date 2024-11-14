fa4daf35a2aacdec5f1dd0e30765af26ba1be79c
fcbento <felipe.16costa@gmail.com>
2024-11-13 23:22:46 -0300

    delete, regex

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,23 @@
 
 import { Database } from './database.js';
 import { randomUUID } from 'node:crypto';
+import { buildRoutePath } from './utils/build-route-path.js';
 
 const database = new Database();
 
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const users = database.select('users');
       return res.end(JSON.stringify(users));
     }
   },
-  
+
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const { name, email } = req.body
 
@@ -25,9 +26,18 @@ export const routes = [
         name,
         email
       }
-  
+
       database.insert('users', user)
       return res.writeHead(201).end();
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      database.delete('users', id)
+      return res.writeHead(204).end();
     }
   },
 ]
--- src/server.js
+++ src/server.js
@@ -7,12 +7,16 @@ const server = http.createServer(async (req, res) => {
 	const { method, url } = req
 
 	await json(req, res);
-	
+
 	const route = routes.find(route => {
-		return route.method === method && route.path === url
+		return route.method === method && route.path.test(url)
 	})
-	
-	if(route){
+
+	if (route) {
+		const routeParams = req.url.match(route.path)
+
+		req.params = { ...routeParams.groups }
+
 		return route.handler(req, res)
 	}
 
